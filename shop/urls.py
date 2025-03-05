654e7a311a98535bafe54ee77358081d1139ddeb
silo20 <akila.20230228@iit.ac.lk>
2025-03-05 21:51:10 +0530

    Modified Shop urls.py file

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ItemViewSet, ShopViewSet, update_item, get_shops
+from .views import ItemViewSet, ShopViewSet, update_item, get_shops, add_item
 
 router = DefaultRouter()
 router.register(r'items', ItemViewSet)
@@ -9,5 +9,6 @@ router.register(r'shops', ShopViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('update_item/<int:item_id>/', update_item, name='update_item'),
+    path('add_item/<int:item_id>/', add_item, name='add_item'),
     path('get_shops/', get_shops, name='get_shops'),
 ]
