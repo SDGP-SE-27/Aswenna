d445a8e7cc3149cf3563b0f34e06fd8aa142f150
silo20 <akila.20230228@iit.ac.lk>
2025-03-05 21:50:37 +0530

    Modified Shop serializers.py file

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
-from .models import Shop, Item
+from .models import Shop, Item, AddItem
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
+        fields = '__all__'
+
+class AddItemSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = AddItem
         fields = '__all__'
 
 class ShopSerializer(serializers.ModelSerializer):
