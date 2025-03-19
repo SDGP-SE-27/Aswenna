ba466a2f35e61bf56f454dd3dce1dc1a41593836
Ruchira De Silva <ruchiradesilva6@gmail.com>
2025-03-19 16:57:40 +0530

    Adding the market forecasting admin page with CRUD operations

---
+++ frontend-marketforecastadmin/admin_page/app/component/layout/page-layout.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+// import { SidebarMain } from "./sidebar-demo";
+// import { Sidebar } from "./simple-sidebar";
+
+// import { Footer } from "./footer";
+// import { Navbar } from "./navbar";
+// import { BackgroundPaths } from "../ui/floating-paths";
+
+export function PageLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="relative min-h-screen">
+      {/* <div className="fixed inset-0 z-0">
+        <BackgroundPaths />
+      </div> */}
+      <div className="relative z-10">
+        {/* <Navbar /> */}
+        {/* <SidebarMain /> */}
+        <main className="min-h-screen pt-16">{children}</main>
+        {/* <Footer /> */}
+      </div>
+    </div>
+  );
+}
